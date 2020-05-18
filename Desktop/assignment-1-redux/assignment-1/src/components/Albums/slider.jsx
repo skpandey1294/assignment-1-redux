5756840bead4eaa50e620ebc6dcc0eb3df0bbe61
Sumit Pandey <skpandey12941@gmail.com>
2020-05-18 14:58:44 +0530

    Corousel with keyframes

--- Desktop/assignment-1-redux/assignment-1/src/components/Albums/slider.jsx
+++ Desktop/assignment-1-redux/assignment-1/src/components/Albums/slider.jsx
@@ -1,40 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../../assets/styles/Slider.css'
-import { slideArrow } from '../../config'
+
+import { keyframes } from 'styled-components'
 
 function Slider({ images }){
 
-    const [xAxis, setX] = useState(0)
 
+    const length = 100/images.length
+
+    let totalTansition = images.map((image, index) => {
+        return`${length * index}%  { transform: translateX(-${100*index}%)}` })
+
+    let slideShow = keyframes`
+    ${totalTansition}
+    `
 
         let slider = images.map((image, index) => {
-            console.log(image)
             return (
-                <div key={index} className="slide" style={{ transform: `translateX(${xAxis}%)`}}>
+                <div key={index} className="slide" style={{ animation: `${slideShow} ${images.length * 4}s ease-out 1s infinite`}}>
                     <img className="img" src={image.url} alt="slide-img"></img>
                 </div>
             )
         })
 
-        const prev = () => {
-            xAxis === 0 ? setX(-100 * (slider.length - 1)) : setX(xAxis + 100)
-        }
-
-        const next = () => {
-            xAxis === -100 * (slider.length - 1) ? setX(0) : setX(xAxis - 100)
-        }
-
 
         return (
             <div className="slider">
                 {slider}
-                <button className="prev" onClick={prev}>
-                    <img src={`${slideArrow}left.png`} alt="Previous" />
-                </button>
-
-                <button className="next" onClick={next}>
-                    <img src={`${slideArrow}right.png`} alt="Next" />
-                </button>
             </div>
         )
     }
